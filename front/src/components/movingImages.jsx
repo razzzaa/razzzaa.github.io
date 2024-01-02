fa69422dc0356814852bef4161b751f21e97b109
Yuri <yurikogan194@gmail.com>
2024-01-02 15:53:32 +0200

    imageFixOne

--- front/src/components/movingImages.jsx
+++ front/src/components/movingImages.jsx
@@ -5,15 +5,18 @@ import ImageListItemBar from "@mui/material/ImageListItemBar";
 function MovingImages(props) {
   const { country, img_name } = props.movingData;
   console.log(img_name);
+  console.log(country);
+
   const imageArray = img_name.split(",");
+  console.log(imageArray);
   const mainImage = imageArray.find((imageName) =>
     imageName.includes("MI.jpg")
   );
   return (
     <ImageListItem key={country}>
       <img
-        src={`/images/countriesImg/${country}/${mainImage}?w=248&fit=crop&auto=format`}
-        srcSet={`/images/countriesImg/${country}/${mainImage}?w=248&fit=crop&auto=format&dpr=6 2x`}
+        src={`images/countriesImg/${country}/${mainImage}?w=248&fit=crop&auto=format`}
+        srcSet={`images/countriesImg/${country}/${mainImage}?w=248&fit=crop&auto=format&dpr=6 2x`}
         alt={country}
         loading="lazy"
       />
